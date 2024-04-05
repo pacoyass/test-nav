c34a59dd933e3e35a6309fea49184e36da0a13a8
pacoyass <74764650+pacoyass@users.noreply.github.com>
2024-04-05 00:23:51 +0000

    latest

--- app/page.js
+++ app/page.js
@@ -15,10 +15,12 @@ export default function Home() {
 
  <section className="flex items-center justify-center py-20">
 <h1 className="text-3xl md:text-5xl lg:text-7xl 2xl:text-9xl">Menus</h1>
-</section>  
+</section> 
+<Separator  className="w-[90%] mx-auto my-4 rounded-2xl bg-neutral-700"/>  
 <section className="flex items-center justify-center py-20">
 <h1 className="text-3xl md:text-5xl lg:text-7xl 2xl:text-9xl">Promotions</h1>
 </section> 
+<Separator  className="w-[90%] mx-auto my-4 rounded-2xl bg-neutral-700"/> 
 <section className="flex items-center justify-center py-20">
 <h1 className="text-3xl md:text-5xl lg:text-7xl 2xl:text-9xl">Livraison</h1>
 </section> 
